d8bfe68633b4b6f34db20fc55985099d66c29ffb
gcdavis26 <geoffreycdavis@proton.me>
2025-04-20 20:38:42 -0400

    fixed constraint issues

--- Optimization.py
+++ Optimization.py
@@ -7,74 +7,60 @@ class Optimization:
         self.constraints = []
         self.log = []
         self.add_geom_con()
-    def cl(self,cst):
+    def update(self,cst):
         if np.all(self.solver.getValuesNp() == cst):
-            funcs = self.solver.funcs
+            pass
         else:
             self.solver.updateCSTCoeff(cst)
-            funcs = self.solver.solvePrimal()
+            self.solver.solvePrimal()
+            self.solver.findFunctionSens()
+            self.solver.findConstraint()
+            self.solver.findConSens()
+
+    def cl(self,cst):
+        self.update(cst)
+        funcs = self.solver.funcs
         cl = funcs["fc_cl"]
         return cl
 
     def cd(self,cst):
-        if np.all(self.solver.getValuesNp() == cst):
-            funcs = self.solver.funcs
-        else:
-            self.solver.updateCSTCoeff(cst)
-            funcs = self.solver.solvePrimal()
+        self.update(cst)
+        funcs = self.solver.funcs
         cd = funcs["fc_cd"]
         return cd
 
     def cd_grad(self,cst):
-        if np.all(self.solver.getValuesNp() == cst):
-            grads = self.solver.func_sens
-        else:
-            self.solver.updateCSTCoeff(cst)
-            grads = self.solver.findFunctionSens()
+        self.update(cst)
+        grads = self.solver.func_sens
         fc_cd = grads['fc_cd']
         combined = fc_cd['upper_shape'].tolist() + fc_cd['lower_shape'].tolist()
         return np.array(combined)
 
     def cl_grad(self,cst):
-        if np.all(self.solver.getValuesNp() == cst):
-            grads = self.solver.func_sens
-        else:
-            self.solver.updateCSTCoeff(cst)
-            grads = self.solver.findFunctionSens()
+        self.update(cst)
+        grads = self.solver.func_sens
         fc_cl = grads['fc_cl']
         combined = fc_cl['upper_shape'].tolist() + fc_cl['lower_shape'].tolist()
         return np.array(combined)
 
     def thickness(self,cst):
-        if np.all(self.solver.getValuesNp() == cst):
-            funcs = self.solver.thickness
-        else:
-            self.solver.updateCSTCoeff(cst)
-            funcs = self.solver.findConstraint()[0]
+        self.update(cst)
+        funcs = self.solver.thickness
         return funcs #200 by 1
 
     def thickness_grad(self,cst):
-        if np.all(self.solver.getValuesNp() == cst):
-            grads = self.solver.thickness_sens
-        else:
-            self.solver.updateCSTCoeff(cst)
-            grads = self.solver.findConSens()[0]
+        self.update(cst)
+        grads = self.solver.thickness_sens
         return grads #200 by 8
 
     def radius(self,cst):
-        if np.all(self.solver.getValuesNp() == cst):
-            funcs = self.solver.le
-        else:
-            self.solver.updateCSTCoeff(cst)
-            funcs = self.solver.findConstraint()[1]
+        self.update(cst)
+        funcs = self.solver.le
         return funcs #2 by 1
 
     def radius_grad(self,cst):
-        if np.all(self.solver.getValuesNp() == cst):
-            grads = self.solver.le_sens
-        else:
-            self.solver.updateCSTCoeff(cst)
-            grads = self.solver.findConSens()[1]
+        self.update(cst)
+        grads = self.solver.le_sens
         return grads #2 by 8 
     def add_con(self, func, jac, con_type): #need to pass a lambda function or some function with inputs
         constraint = {'type': con_type,
@@ -82,21 +68,27 @@ class Optimization:
              'jac': jac}
         self.constraints.append(constraint)
     def add_geom_con(self):
-        for iter in range(200):
-            func = lambda cst: self.thickness(cst)[iter]
-            jac = lambda cst: self.thickness_grad(cst)[iter,:]
+        for it in range(200):
+            def func(cst,index=it):
+                return self.thickness(cst)[index]
+            def jac(cst,index=it):
+                return self.thickness_grad(cst)[index,:]
             self.add_con(func,jac,'ineq')
-        for iter in range(2):
-            func = lambda cst: self.radius(cst)[iter]
-            jac = lambda cst: self.radius_grad(cst)[iter,:]
+        for it in range(2):
+            def func(cst,index=it):
+                return self.radius(cst)[index]
+            def jac(cst,index=it):
+                return self.radius_grad(cst)[index,:]
             self.add_con(func,jac,'ineq')
     def slsqp(self, bounds): #bounds must be Bounds object
         def callback(cst):
             self.log.append(cst)
             print(f"Cl: {self.constraints[len(self.constraints)-1]['fun'](cst)}")
             #for con in self.constraints:
-            #    print(cst)
-            #    print(f"Constraint: {con['fun'](cst)}. Func:{self.cd(cst)}")
+             #   print(f"Constraint: {con['fun'](cst)}")   
+            #thicknesses = self.thickness(cst) 
+            #for x in range(200):
+            #    print(thicknesses[x])
                 
         res = minimize(self.cd, self.solver.getValuesNp(), method = "SLSQP", jac=self.cd_grad,
                constraints=self.constraints, tol=1e-6,
@@ -104,16 +96,16 @@ class Optimization:
     def trustcr(self,bounds):
         def callback(cst, state):
             self.log.append(cst)
+            print(f"Cl: {self.constraints[len(self.constraints)-1]['fun'](cst)}")
             #for con in self.constraints:
-            #    print(cst)
-            #    print(f"Constraint: {con['fun'](cst)}. Func:{self.cd(cst)}")
-            
+                #print(cst)
+                #print(f"Constraint: {con['fun'](cst)}")    
         res = minimize(self.cd, self.solver.getValuesNp(), method = "trust-constr", jac=self.cd_grad,
             constraints=self.constraints, tol=1e-6,
             bounds=bounds,callback = callback)
     def cobyqa(self,bounds):
         def callback(cst):
-            pass #add stuff here
+            print(f"Cl: {self.constraints[len(self.constraints)-1]['fun'](cst)}")
         res = minimize(self.cd, self.solver.getValuesNp(), method = "COBYQA",
             constraints=self.constraints, tol=1e-6,
             bounds=bounds,callback = callback)
